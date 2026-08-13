0c12df7203bfa5262b20b2383f9199babe2a1266
Miauczyx <74237486+Miauczyx@users.noreply.github.com>
2026-08-13 17:18:02 +0200

    Update tournament-data.js

--- tournament-data.js
+++ tournament-data.js
@@ -12,7 +12,6 @@ const teams = [
     ["Team Psycho", 2572, "psycho.png"],
     ["Open Net Missers", 2563, "open.png"],
     ["dom starców bydgoszcz", 2528, "dom.png"],
-    ["NoMerc", 2511, "logo.png"],
     ["Peakers", 2500, "peakers.png"],
     ["Gta6", 2498, "gta.png"],
     ["112", 2498, "112.png"],
@@ -35,19 +34,20 @@ const teams = [
     ["porzeczki", 1857, "porzeczki.png"],
     ["Rzurek", 1772, "rzurek.png"],
     ["Poszukiwacze Złomu", 1768, "zlom.png"],
-    ["Cwordy", 1408, "cwele.jpg"]
+    ["Cwordy", 1408, "cwele.jpg"],
+    ["Jajo", 0, "jajo.gif"]
 ];
 
 const swissMatches = {
     s1_round1: [
-        { team1: "Czekotubki", team2: "Cwordy", score1: 0, score2: 0 },
+        { team1: "Czekotubki", team2: "Jajo", score1: 0, score2: 0 },
         { team1: "Szybcy i Biedni", team2: "Konduktorzy", score1: 0, score2: 0 },
-        { team1: "Gta6", team2: "Genziary", score1: 0, score2: 0 },
+        { team1: "112", team2: "inazuma esports", score1: 0, score2: 0 },
         { team1: "Open Net Missers", team2: "Obrotni", score1: 0, score2: 0 },
-        { team1: "Zlew", team2: "ogboss", score1: 0, score2: 0 },
-        { team1: "NoMerc", team2: "Banda Bandaży", score1: 0, score2: 0 },
-        { team1: "Prorocy", team2: "Normalni Ludzie", score1: 0, score2: 0 },
-        { team1: "Black Horse", team2: "Rzurek", score1: 0, score2: 0 }
+        { team1: "Zlew", team2: "porzeczki", score1: 0, score2: 0 },
+        { team1: "Peakers", team2: "team overdrive", score1: 0, score2: 0 },
+        { team1: "BRACIA KLOPS", team2: "Normalni Ludzie", score1: 0, score2: 0 },
+        { team1: "Black Horse", team2: "Poszukiwacze Złomu", score1: 0, score2: 0 }
     ],
 
     s1_round2_1_0: [
@@ -128,14 +128,14 @@ const swissMatches = {
     ],
 
     s2_round1: [
-        { team1: "złotówka team", team2: "Poszukiwacze Złomu", score1: 0, score2: 0 },
-        { team1: "Szybcy i Biedni", team2: "Aero Esport", score1: 0, score2: 0 },
-        { team1: "112", team2: "Normalni Ludzie", score1: 0, score2: 0 },
+        { team1: "złotówka team", team2: "Cwordy", score1: 0, score2: 0 },
+        { team1: "Aero Esport", team2: "Konduktorzy", score1: 0, score2: 0 },
+        { team1: "Prorocy", team2: "Genziary", score1: 0, score2: 0 },
         { team1: "dom starców bydgoszcz", team2: "fc Barcelona", score1: 0, score2: 0 },
         { team1: "Team Psycho", team2: "ogboss", score1: 0, score2: 0 },
-        { team1: "Peakers", team2: "inazuma esports", score1: 0, score2: 0 },
-        { team1: "BRACIA KLOPS", team2: "fc nikt nas nie chce", score1: 0, score2: 0 },
-        { team1: "Niezbyt szybcy i spokojni", team2: "porzeczki", score1: 0, score2: 0 }
+        { team1: "Gta6", team2: "Banda Bandaży", score1: 0, score2: 0 },
+        { team1: "bajo", team2: "wyjadacze", score1: 0, score2: 0 },
+        { team1: "Niezbyt szybcy i spokojni", team2: "Rzurek", score1: 0, score2: 0 }
     ],
 
     s2_round2_1_0: [
