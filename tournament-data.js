4daddd460f573cd3e11f19e1bf3911fe4864af4f
kamilox364 <roznerrkamil@gmail.com>
2026-08-13 18:31:46 +0200

    Update tournament-data.js

--- tournament-data.js
+++ tournament-data.js
@@ -41,7 +41,7 @@ const teams = [
 const swissMatches = {
     s1_round1: [
         { team1: "Czekotubki", team2: "Cwordy", score1: 2, score2: 0 },
-        { team1: "Szybcy i Biedni", team2: "fc nikt nas nie chce", score1: 0, score2: 0 },
+        { team1: "Szybcy i Biedni", team2: "fc nikt nas nie chce", score1: 2, score2: 0 },
         { team1: "112", team2: "inazuma esports", score1: 0, score2: 0 },
         { team1: "Open Net Missers", team2: "Obrotni", score1: 0, score2: 0 },
         { team1: "Zlew", team2: "porzeczki", score1: 2, score2: 0 },
