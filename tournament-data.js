9c968623603bd4b2c8db2b81b1f07260dba0a23b
Miauczyx <74237486+Miauczyx@users.noreply.github.com>
2026-08-13 14:56:42 +0200

    Update tournament-data.js

--- tournament-data.js
+++ tournament-data.js
@@ -11,8 +11,8 @@ const teams = [
     ["Zlew", 2584, "zlew.png"],
     ["Team Psycho", 2572, "psycho.png"],
     ["Open Net Missers", 2563, "open.png"],
-    ["NoMerc", 2511, "logo.png"],
     ["dom starców bydgoszcz", 2528, "dom.png"],
+    ["NoMerc", 2511, "logo.png"],
     ["Peakers", 2500, "peakers.png"],
     ["112", 2498, "112.png"],
     ["Prorocy", 2482, "prorocy.jpg"],
@@ -38,156 +38,178 @@ const teams = [
     ["Cwordy", 1408, "cwele.jpg"]
 ];
 
-// ============================================
-//  WYNIKI MECZÓW - SWISS STAGE
-// ============================================
-
 const swissMatches = {
-    // === SWISS GROUP 1 ===
     s1_round1: [
         { team1: "Czekotubki", team2: "Cwordy", score1: 0, score2: 0 },
-        { team1: "Black Horse", team2: "Poszukiwacze Złomu", score1: 0, score2: 0 },
-        { team1: "Zlew", team2: "Rzurek", score1: 0, score2: 0 },
-        { team1: "Open Net Missers", team2: "porzeczki", score1: 0, score2: 0 },
-        { team1: "dom starców bydgoszcz", team2: "ogboss", score1: 0, score2: 0 },
-        { team1: "112", team2: "BDJQP", score1: 0, score2: 0 },
-        { team1: "BRACIA KLOPS", team2: "Obrotni", score1: 0, score2: 0 },
-        { team1: "Szybcy i Biedni", team2: "fc Barcelona", score1: 0, score2: 0 }
+        { team1: "Szybcy i Biedni", team2: "wyjadacze", score1: 0, score2: 0 },
+        { team1: "112", team2: "Wina Tuska", score1: 0, score2: 0 },
+        { team1: "Open Net Missers", team2: "Obrotni", score1: 0, score2: 0 },
+        { team1: "Zlew", team2: "ogboss", score1: 0, score2: 0 },
+        { team1: "NoMerc", team2: "team overdrive", score1: 0, score2: 0 },
+        { team1: "BRACIA KLOPS", team2: "Genziary", score1: 0, score2: 0 },
+        { team1: "Black Horse", team2: "Rzurek", score1: 0, score2: 0 }
     ],
+
     s1_round2_1_0: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round2_0_1: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round3_2_0: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round3_1_1: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round3_0_2: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round4_3_0: [
         { team1: "TBD", team2: "TBD" }
     ],
+
     s1_round4_2_1: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round4_1_2: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round4_0_3: [
         { team1: "TBD", team2: "TBD" }
     ],
+
     s1_round5_3_1: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
+
     s1_round5_2_2: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s1_round5_1_3: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
+
     s1_round6_3_2: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
+
     s1_round6_2_3: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
 
-    // === SWISS GROUP 2 ===
     s2_round1: [
         { team1: "złotówka team", team2: "Poszukiwacze Złomu", score1: 0, score2: 0 },
-        { team1: "Niezbyt szybcy i spokojni", team2: "Rzurek", score1: 0, score2: 0 },
-        { team1: "Team Psycho", team2: "porzeczki", score1: 0, score2: 0 },
-        { team1: "NoMerc", team2: "ogboss", score1: 0, score2: 0 },
-        { team1: "Peakers", team2: "BDJQP", score1: 0, score2: 0 },
-        { team1: "Prorocy", team2: "Obrotni", score1: 0, score2: 0 },
-        { team1: "bajo", team2: "fc Barcelona", score1: 0, score2: 0 },
-        { team1: "Aero Esport", team2: "team overdrive", score1: 0, score2: 0 }
+        { team1: "Aero Esport", team2: "Konduktorzy", score1: 0, score2: 0 },
+        { team1: "Prorocy", team2: "inazuma esports", score1: 0, score2: 0 },
+        { team1: "dom starców bydgoszcz", team2: "fc Barcelona", score1: 0, score2: 0 },
+        { team1: "Team Psycho", team2: "BDJQP", score1: 0, score2: 0 },
+        { team1: "Peakers", team2: "Banda Bandaży", score1: 0, score2: 0 },
+        { team1: "bajo", team2: "Normalni Ludzie", score1: 0, score2: 0 },
+        { team1: "Niezbyt szybcy i spokojni", team2: "porzeczki", score1: 0, score2: 0 }
     ],
+
     s2_round2_1_0: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round2_0_1: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round3_2_0: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round3_1_1: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round3_0_2: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round4_3_0: [
         { team1: "TBD", team2: "TBD" }
     ],
+
     s2_round4_2_1: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round4_1_2: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round4_0_3: [
         { team1: "TBD", team2: "TBD" }
     ],
+
     s2_round5_3_1: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
+
     s2_round5_2_2: [
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
+
     s2_round5_1_3: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
+
     s2_round6_3_2: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
     ],
+
     s2_round6_2_3: [
         { team1: "TBD", team2: "TBD", team3: "TBD" },
         { team1: "TBD", team2: "TBD", team3: "TBD" }
