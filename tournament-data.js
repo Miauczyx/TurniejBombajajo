0a92d0dd5f702c3fdc0967a39c6ee4d4388f5265
kamilox364 <roznerrkamil@gmail.com>
2026-08-14 18:55:43 +0200

    Update tournament-data.js

--- tournament-data.js
+++ tournament-data.js
@@ -229,7 +229,7 @@ const groupMatches = {
             { name: "inazuma esports", record: "0-1", games: "0-3", points: -3 }
         ],
         matches: [
-            { team1: "Open Net Missers", team2: "Black Horse", score1: 0, score2: 0 },
+            { team1: "Open Net Missers", team2: "Black Horse", score1: 2, score2: 3 },
             { team1: "bajo", team2: "inazuma esports", score1: 3, score2: 0 },
             { team1: "Open Net Missers", team2: "bajo", score1: 0, score2: 0 },
             { team1: "Black Horse", team2: "inazuma esports", score1: 0, score2: 0 },
