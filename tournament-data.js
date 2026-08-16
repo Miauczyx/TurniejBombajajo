0cef5adbd90bdf9f501f4da51607e4b4371cc866
kamilox364 <roznerrkamil@gmail.com>
2026-08-16 19:59:46 +0200

    Update tournament-data.js

--- tournament-data.js
+++ tournament-data.js
@@ -307,7 +307,7 @@ const playoffMatches = {
         { team1: "złotówka team", team2: "Zlew", score1: 4, score2: 2 }
     ],
     lbqf: [
-        { team1: "TBD", team2: "TBD", score1: 0, score2: 0 },
+        { team1: "Zlew", team2: "TBD", score1: 0, score2: 0 },
         { team1: "TBD", team2: "TBD", score1: 0, score2: 0 }
     ],
     semifinals: [
