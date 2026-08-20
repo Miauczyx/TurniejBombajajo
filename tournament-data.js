960c03696b0e5b98d41b93c53fef36738c46146b
Miauczyx <74237486+Miauczyx@users.noreply.github.com>
2026-08-20 21:07:58 +0200

    Update tournament-data.js

--- tournament-data.js
+++ tournament-data.js
@@ -300,7 +300,7 @@ const playoffMatches = {
     ],
     lbr2: [
         { team1: "Black Horse", team2: "team overdrive", score1: 0, score2: 0 },
-        { team1: "fc nikt nas nie chce", team2: "Open Net Missers", score1: 0, score2: 0 }
+        { team1: "fc nikt nas nie chce", team2: "Open Net Missers", score1: 1, score2: 4 }
     ],
     ubqf: [
         { team1: "bajo", team2: "Niezbyt szybcy i spokojni", score1: 4, score2: 2 },
@@ -308,7 +308,7 @@ const playoffMatches = {
     ],
     lbqf: [
         { team1: "Zlew", team2: "TBD", score1: 0, score2: 0 },
-        { team1: "Niezbyt szybcy i spokojni", team2: "TBD", score1: 0, score2: 0 }
+        { team1: "Niezbyt szybcy i spokojni", team2: "Open Net Missers", score1: 0, score2: 0 }
     ],
     semifinals: [
         { team1: "bajo", team2: "TBD", score1: 0, score2: 0 },
